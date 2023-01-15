062042e72f4ab3c27ba6802d4ec5e7a52e6536f4
koseckaya <interika11@gmail.com>
2023-01-15 20:57:34 +0200

    feat: add services connect to back

---
+++ src/services/APIService.ts
@@ -0,0 +1,38 @@
+// @ts-nocheck
+
+const BACKEND_URL = 'http://localhost:3000/';
+const ENDPOINTS = {
+    'ENGINE': BACKEND_URL + 'engine',
+    'GARAGE': BACKEND_URL + 'garage',
+    'WINNERS': BACKEND_URL + 'winners'
+}
+
+export const getCarsList = () => {
+    return fetch(ENDPOINTS.GARAGE, {
+        method: 'GET',
+    })
+}
+
+export const createCar = (params) => {
+    return fetch(ENDPOINTS.GARAGE, {
+        method: 'POST',
+        body: JSON.stringify(params),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((data) => data.json())
+}
+
+export const deleteCar = (id) => {
+    return fetch(ENDPOINTS.GARAGE +'/'+ id, {
+        method: 'DELETE',
+    })
+}
+
+
+
+export const getWinnersList = () => {
+    return fetch(ENDPOINTS.WINNERS, {
+        method: 'GET',
+    }).then((data) => data.json())
+}
---
+++ src/services/ListServices.ts
@@ -0,0 +1,64 @@
+//@ts-nocheck
+class ListServices {
+    items = []
+    page = 1
+    perPage = 7
+    constructor (items, perPage) {
+        this.items = items;
+        this.perPage = perPage;
+    }
+
+    setPage = (num) => {
+        if (num < 1 || num > this.getTotalPages()) {
+            this.page = 1
+        } else {
+            this.page = num
+        }
+    }
+
+    getDataByCurrentPage = () => { 
+        const startWith = (this.page - 1) * this.perPage
+        const itemsOnPage = this.items.slice(startWith, startWith + this.perPage)
+        return itemsOnPage;
+    }
+
+    setItems = (items) => {
+        this.items = items
+    }
+
+    getTotal = () => this.items.length;
+
+    getPage = () => this.page;
+
+    getTotalPages = () => Math.ceil(this.getTotal() / this.perPage);
+    
+    addEntity = (entity) => this.items.push(entity)
+
+    updateEntity = (id, entity) => {
+        const newItems = this.items.map((item) => {
+            if (item.id === id) {
+                return { ...item, ...entity}
+            } 
+            return item;
+        })
+        this.items = newItems;
+    }
+
+    removeEntity = (id) => { 
+        this.items = this.items.filter((item) => {
+            return item.id !== id;
+        } )
+    }
+    
+    getEntity = (id) => this.items.find((item) => item.id === id);
+  
+    isFirstPage = () => {
+        return this.getPage() === 1 ? true : false;
+    }
+    isLastPage = () => {
+        return this.getPage() === this.getTotalPages() ? true : false;
+    }
+
+
+}
+export default ListServices
